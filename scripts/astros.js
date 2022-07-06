fed0f4e25c941282b302a3eacf026c00b0a45ea6
Diego <luizdiego.silvabarbosa@gmail.com>
2022-07-06 04:55:13 -0300

    Iniciando implementação do slider

--- scripts/astros.js
+++ scripts/astros.js
@@ -1,17 +1,59 @@
+// Atualizar informações de cada astro ----------------------------------------
+function atualizarInformacoesAstro ( event )
+{
+    let botaoProximo = event.target.classList.contains( "carousel-control-next-icon" );
+    let valorIncremento = botaoProximo ? 1 : -1;
 
+    indiceAtual += valorIncremento;
+    console.log( indiceAtual );
 
-// function proximoAstro
-// {
+    return
+    astroNome.innerText = listaAstroInformacoes[ indiceAtual ].nome;
+    astroDescricao.innerText = listaAstroInformacoes[ indiceAtual ].descricao;
+    astroNome.tamanho = listaAstroInformacoes[ indiceAtual ].tamanho;
+    astroNome.temperatura = listaAstroInformacoes[ indiceAtual ].temperatura;
+    astroNome.gravidade = listaAstroInformacoes[ indiceAtual ].gravidade;
 
-// }
+}
 
-// function astroAnterior
-// {
+const listaAstroInformacoes =
+[
+    {
+        nome: "Terra",
+        descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur quo doloremque labore, doloribus illum molestiae natus dignissimos impedit. Ut laborum consequatur aut ipsum. Consequatur eveniet officia adipisci, possimus fugiat nisi.",
+        tamanho: "0000",
+        temperatura: "0000",
+        gravidade: "0000",
+    },
+    {
+        nome: "Lua",
+        descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur quo doloremque labore, doloribus illum molestiae natus dignissimos impedit. Ut laborum consequatur aut ipsum. Consequatur eveniet officia adipisci, possimus fugiat nisi.",
+        tamanho: "0000",
+        temperatura: "0000",
+        gravidade: "0000",
+    },
+    {
+        nome: "Marte",
+        descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur quo doloremque labore, doloribus illum molestiae natus dignissimos impedit. Ut laborum consequatur aut ipsum. Consequatur eveniet officia adipisci, possimus fugiat nisi.",
+        tamanho: "0000",
+        temperatura: "0000",
+        gravidade: "0000",
+    },
+]
 
-// }
+const astroNome = document.getElementById( "astro-nome" );
+const astroDescricao = document.getElementById( "astro-descricao" );
+const astroTamanho = document.getElementById( "astro-tamanho" );
+const astroTemperatura = document.getElementById( "astro-temperatura" );
+const astroGravidade = document.getElementById( "astro-gravidade" );
 
-// let totalAtros = document.getElementsByClassName().length;
-// let indiceAtual = 0;
+const botaoProximo = document.getElementById( "astro-proximo" );
+botaoProximo.addEventListener( "click", atualizarInformacoesAstro );
 
+const botaoAnterior = document.getElementById( "astro-anterior" );
+botaoAnterior.addEventListener( "click", atualizarInformacoesAstro );
 
-
+let totalAstros = document.getElementsByClassName( "astro" ).length;
+let indiceAtual = 0;
+console.log( indiceAtual )
+console.log( totalAstros )
