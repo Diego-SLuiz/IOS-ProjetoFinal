f5b84dfbd6b83f636f768e54c2d35b7127897c09
Brenno10 <brenno10000@outlook.com>
2022-07-07 14:41:01 -0300

    criando esboço de animação da orbita dos planetas

--- scripts/planos.js
+++ scripts/planos.js
@@ -54,3 +54,98 @@ function escreverInfo() {
     infoImg.innerHTML = imgTeste;
     infoTexto.innerHTML = textoTeste;
 }
+// fim modal
+
+// astros
+const sun = document.querySelector('#sun');
+const mercury = {
+    speed: 0.00005,
+    theta: Math.random() * Math.PI * 2,
+    radius: 100,
+    el: document.querySelector('#mercury'),
+};
+
+const venus = {
+    speed: 0.00005,
+    theta: Math.random() * Math.PI * 2,
+    radius: 150,
+    el: document.querySelector('#venus'),
+};
+
+const earth = {
+    speed: 0.00005,
+    theta: Math.random() * Math.PI * 2,
+    radius: 210,
+    el: document.querySelector('#earth'),
+};
+
+const mars = {
+    speed: 0.00005,
+    theta: Math.random() * Math.PI * 2,
+    radius: 280,
+    el: document.querySelector('#mars'),
+};
+
+const jupiter = {
+    speed: 0.00005,
+    theta: Math.random() * Math.PI * 2,
+    radius: 340,
+    el: document.querySelector('#jupiter'),
+};
+
+const saturn = {
+    speed: 0.00005,
+    theta: Math.random() * Math.PI * 2,
+    radius: 450,
+    el: document.querySelector('#saturn'),
+};
+
+const uranus = {
+    speed: 0.00005,
+    theta: Math.random() * Math.PI * 2,
+    radius: 540,
+    el: document.querySelector('#uranus'),
+};
+
+const neptune = {
+    speed: 0.00005,
+    theta: Math.random() * Math.PI * 2,
+    radius: 620,
+    el: document.querySelector('#neptune'),
+};
+
+const planets = [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune];
+
+// posiciona o sol no centro da tela
+var sunX = window.innerWidth / 2 - sun.clientWidth / 2;
+var sunY = window.innerHeight / 2 - sun.clientHeight / 2;
+sun.style.left = `${sunX}px `;
+sun.style.top = `${sunY}px `;
+
+// reposiciona o sol no centro caso o tamahno da tela mude
+window.addEventListener('resize', redimensionar);
+function redimensionar() {
+    sunX = window.innerWidth / 2 - sun.clientWidth / 2;
+    sunY = window.innerHeight / 2 - sun.clientHeight / 2;
+    sun.style.left = `${sunX}px `;
+    sun.style.top = `${sunY}px `;
+}
+
+// atualiza a posição dos planetas em volta do sol
+function update(planet) {
+    planet.theta += planet.speed;
+    planet.el.style.left = `${
+        Math.cos(planet.theta) * planet.radius + sunX + sun.clientWidth / 2
+    }px `;
+    planet.el.style.top = `${
+        Math.sin(planet.theta) * planet.radius + sunY + sun.clientHeight / 2
+    }px `;
+}
+
+// renderiza tudo na página
+function render() {
+    requestAnimationFrame(render);
+    planets.forEach(update);
+}
+render();
+// fim astros
