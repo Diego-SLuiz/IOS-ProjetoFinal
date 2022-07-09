9ece5d7abf8592932dc2d8746de21a3e5176c8eb
Diego <luizdiego.silvabarbosa@gmail.com>
2022-07-09 20:58:56 -0300

    Conectando páginas e corrigindo posição do footer

--- scripts/header.js
+++ scripts/header.js
@@ -1,14 +1,14 @@
 // create a header
 let header = document.createElement('header');
 
-//make the HTML template to insert inside your header
+// make the HTML template to insert inside your header
 let headerContent = `
     <nav id="menu-container" class="navbar navbar-expand-lg justify-content-between ">
-    <a href="">
-        <img src="../assets/images/titulo-desktop.png" alt="Título Desktop" id="menu-imagem-desktop" class="d-none d-sm-inline" >
-        <img src="../assets/images/titulo-mobile.png" alt="Título Mobile" id="menu-imagem-mobile" class="d-inline d-sm-none" >
-    </a>
-        
+        <a href="../pages/inicio.html">
+            <img src="../assets/images/titulo-desktop.png" alt="Título Desktop" id="menu-imagem-desktop" class="d-none d-sm-inline" >
+            <img src="../assets/images/titulo-mobile.png" alt="Título Mobile" id="menu-imagem-mobile" class="d-inline d-sm-none" >
+        </a>
+
         <button
             class="navbar-toggler"
             type="button"
@@ -21,17 +21,38 @@ let headerContent = `
         </button>
 
         <div id="menu-navegacao" class="collapse navbar-collapse navbar-nav justify-content-lg-center">
-        
-            <a href="" class="menu-item nav-item hover menu-ativo"><span class="menu-numero">00</span>Início</a>
-            <a href="" class="menu-item nav-item hover "><span class="menu-numero">01</span>Curiosidades</a>
-            <a href="" class="menu-item nav-item hover "><span class="menu-numero">02</span>Destinos</a>
-            <a href="" class="menu-item nav-item hover "><span class="menu-numero">03</span>Sobre</a>
-        
+            <a
+                id="menu-inicio"
+                href="../pages/inicio.html"
+                class="menu-item nav-item hover">
+                <span class="menu-numero">00</span>Início
+            </a>
+
+            <a
+                id="menu-curiosidades"
+                href="../pages/astros.html"
+                class="menu-item nav-item hover ">
+                <span class="menu-numero">01</span>Curiosidades
+            </a>
+
+            <a
+                id="menu-planos"
+                href="../pages/planos.html"
+                class="menu-item nav-item hover ">
+                <span class="menu-numero">02</span>Destinos
+            </a>
+
+            <a
+                id="menu-sobre"
+                href="../pages/sobre.html"
+                class="menu-item nav-item hover ">
+                <span class="menu-numero">03</span>Sobre
+            </a>
         </div>
     </nav>
 `;
 
-//insert the header right before anything else
+// insert the header right before anything else
 document.querySelector('body').prepend(header);
 
 // fill your header with HTML template
