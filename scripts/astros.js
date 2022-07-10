bea1c2f24a98704338a88be20cd32c141114210a
Diego <luizdiego.silvabarbosa@gmail.com>
2022-07-10 00:38:34 -0300

    Slide com astros funcionando e atualizando informações

--- scripts/astros.js
+++ scripts/astros.js
@@ -1,59 +1,74 @@
-// Atualizar informações de cada astro ----------------------------------------
-function atualizarInformacoesAstro ( event )
+function proximoAstro ()
 {
-    let botaoProximo = event.target.classList.contains( "carousel-control-next-icon" );
-    let valorIncremento = botaoProximo ? 1 : -1;
-
-    indiceAtual += valorIncremento;
-    console.log( indiceAtual );
-
-    return
-    astroNome.innerText = listaAstroInformacoes[ indiceAtual ].nome;
-    astroDescricao.innerText = listaAstroInformacoes[ indiceAtual ].descricao;
-    astroNome.tamanho = listaAstroInformacoes[ indiceAtual ].tamanho;
-    astroNome.temperatura = listaAstroInformacoes[ indiceAtual ].temperatura;
-    astroNome.gravidade = listaAstroInformacoes[ indiceAtual ].gravidade;
-
+    indiceAtual += 1;
+    indiceAtual = indiceAtual > totalAstros - 1 ? 0 : indiceAtual;
+    atualizarInformacoes();
 }
 
+function astroAnterior ()
+{
+    indiceAtual -= 1;
+    indiceAtual = indiceAtual < 0 ? totalAstros - 1 : indiceAtual;
+    atualizarInformacoes();
+}
+
+function atualizarInformacoes ()
+{
+    let tamanho = listaAstroInformacoes[ indiceAtual ].tamanho;
+    let temperatura = listaAstroInformacoes[ indiceAtual ].temperatura;
+    let gravidade = listaAstroInformacoes[ indiceAtual ].gravidade;
+
+    astroNome.innerText = listaAstroInformacoes[ indiceAtual ].nome;
+    astroDescricao.innerText = listaAstroInformacoes[ indiceAtual ].descricao;
+    astroTamanho.childNodes[ 1 ].nodeValue = tamanho;
+    astroTemperatura.childNodes[ 1 ].nodeValue = temperatura;
+    astroGravidade.childNodes[ 1 ].nodeValue = gravidade;
+    console.log( gravidade )
+}
+
+// Array contendo as informações que serão adicionadas
 const listaAstroInformacoes =
 [
     {
         nome: "Terra",
-        descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur quo doloremque labore, doloribus illum molestiae natus dignissimos impedit. Ut laborum consequatur aut ipsum. Consequatur eveniet officia adipisci, possimus fugiat nisi.",
-        tamanho: "0000",
-        temperatura: "0000",
-        gravidade: "0000",
+        descricao: "A Terra é o terceiro planeta mais próximo do Sol, o mais denso e o quinto maior dos oito planetas do Sistema Solar. É também o maior dos quatro planetas telúricos. É por vezes designada como Mundo ou Planeta Azul .",
+        tamanho: "12.742 KM",
+        temperatura: " -60ºC ~ 45ºC",
+        gravidade: "9,8 M/S²",
     },
     {
         nome: "Lua",
-        descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur quo doloremque labore, doloribus illum molestiae natus dignissimos impedit. Ut laborum consequatur aut ipsum. Consequatur eveniet officia adipisci, possimus fugiat nisi.",
-        tamanho: "0000",
-        temperatura: "0000",
-        gravidade: "0000",
+        descricao: "A Lua é o único satélite natural da Terra e o quinto maior do Sistema Solar. É o maior satélite natural de um planeta no sistema solar em relação ao tamanho do seu corpo primário, tendo 27% do diâmetro e 60% da densidade da Terra.",
+        tamanho: "3.474 KM",
+        temperatura: "-184°C ~ 214°C",
+        gravidade: "1,62 M/S²",
     },
     {
         nome: "Marte",
-        descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur quo doloremque labore, doloribus illum molestiae natus dignissimos impedit. Ut laborum consequatur aut ipsum. Consequatur eveniet officia adipisci, possimus fugiat nisi.",
-        tamanho: "0000",
-        temperatura: "0000",
-        gravidade: "0000",
+        descricao: "Marte é o quarto planeta a partir do Sol, o segundo menor do Sistema Solar. Batizado em homenagem ao deus romano da guerra, muitas vezes é descrito como o \"Planeta Vermelho\", porque o óxido de ferro predominante em sua superfície lhe dá uma aparência avermelhada.",
+        tamanho: "6.779 KM",
+        temperatura: "-125°C ~ 22°C",
+        gravidade: "3,7 M/S²",
     },
 ]
 
+// Elementos que terão o conteúdo modificado
 const astroNome = document.getElementById( "astro-nome" );
 const astroDescricao = document.getElementById( "astro-descricao" );
 const astroTamanho = document.getElementById( "astro-tamanho" );
 const astroTemperatura = document.getElementById( "astro-temperatura" );
 const astroGravidade = document.getElementById( "astro-gravidade" );
 
+// Botão para o próximo astro
 const botaoProximo = document.getElementById( "astro-proximo" );
-botaoProximo.addEventListener( "click", atualizarInformacoesAstro );
+botaoProximo.addEventListener( "click", proximoAstro );
 
+// Botão para o astro anterior
 const botaoAnterior = document.getElementById( "astro-anterior" );
-botaoAnterior.addEventListener( "click", atualizarInformacoesAstro );
+botaoAnterior.addEventListener( "click", astroAnterior );
 
+// Indice do astro que está sendo exibido atualmente
 let totalAstros = document.getElementsByClassName( "astro" ).length;
 let indiceAtual = 0;
-console.log( indiceAtual )
-console.log( totalAstros )
+
+atualizarInformacoes();
